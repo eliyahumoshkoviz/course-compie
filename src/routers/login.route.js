131d8281422e45aa68f0c0a5801fec80b7ae399e
= <=>
2024-08-08 17:47:38 +0300

    added רelationship between task to user

--- src/routers/login.route.js
+++ src/routers/login.route.js
@@ -1,20 +1,45 @@
 import express from 'express';
 import { userModel } from '../DL/models/user.model.js';
+import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 
 router.post("/", async (req, res) => {
-
+    
     try {
         const user = new userModel(req.body);
         res.status(201).send({ user, token: await user.generateAuthToken() });
-        await user.save()
 
+    } catch (error) {
+        res.status(500).send({ error: error.message || error.toString() })
+    }
+
+})
+
+router.post("/logout",auth, async (req, res) => {
+
+    try {
+        req.user.tokens = req.user.tokens.filter((t) => t.token !== req.token)
+        await req.user.save()
+        res.status(201).send()
+    } catch (error) {
+        res.status(500).send({ error: error.message || error.toString() })
+    }
+
+})
+
+router.post("/logoutAll",auth, async (req, res) => {
+
+    try {
+
+        req.user.tokens = []
+        await user.save()
+        res.status(201).send()
     } catch (error) {
         res.status(400).send({ error: error.message || error.toString() })
     }
 
-})
+});
 
 export { router as loginRoute };
